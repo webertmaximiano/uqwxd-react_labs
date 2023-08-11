436462f35507dbc185ceb6adfd681e5d98499907
Webert Maximiano <webertcoach@gmail.com>
2023-08-11 07:46:19 -0300

    atualizem as dep

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -5,24 +5,68 @@ const App = () => {
   const [todo, setTodo] = React.useState("");
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0 ) {
+        setTodos([...todos].concat(newTodo));
+        setTodo("");
+
+    } else {
+
+        alert("Enter Valid Task");
+        setTodo("");
+    }
+  }
+
   
   // Add the deleteToDo code here
+
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
 
   
   // Add the toggleComplete code here
 
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
   
   // Add the submitEdits code here
 
   
 return(
 <div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
+
+  <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="Adicionar uma Tarefa"
+            value={todo}
+          />
+              <button type="submit">Adicionar Tarefa</button>
+        </form>
+        {todos.map((todo) => <div className="todo" key={todo.id}>
+        
+        <input type="checkbox" id="completed" checked={todo.completed} onChange={() => toggleComplete(todo.id)}/>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          {todo.text}</div>)}
+
 </div>
 );
 };
